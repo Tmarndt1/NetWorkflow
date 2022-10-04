0d06edb09a15ca403f7881d9f4caaaec3742233d
Travis Arndt <tmarndt1991@gmail.com>
2022-10-04 16:31:05 -0400

    Added NetWorkflow.Extensions

---
+++ NetWorkflow.Extensions/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NetWorkflow.Extensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Registers a Workflow type to the inversion of control services container
+        /// </summary>
+        /// <typeparam name="TWorkflow">The type of workflow to register</typeparam>
+        /// <param name="services">The IServiceCollection to register the Workflow to</param>
+        public static IServiceCollection AddWorkflow<TWorkflow>(this IServiceCollection services)
+            where TWorkflow : class, IWorkflow, new()
+        {
+            return services.AddTransient<TWorkflow>();
+        }
+    }
+}
